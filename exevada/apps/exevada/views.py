baffc3cb0b4e98a4ca0cc903fc9baf04520d2eb4
gvdoord <g.vandenOord@gmail.com>
2020-06-30 12:04:37 +0200

    Added switch to turn off Copernicus wordpress styling

--- exevada/apps/exevada/views.py
+++ exevada/apps/exevada/views.py
@@ -7,24 +7,36 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.contrib.gis import geos
 from .models import Event, Attribution, ObservationDataSet, ModelDataSet, Location
-
+from project.settings.local import WORDPRESS
 
 class Index(ListView):
     template_name = 'exevada/index.html'
     model = Event
     allow_empty = True
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['wp_integrate'] = WORDPRESS
+        return context
 
 
 class EventsView(ListView):
     template_name = 'exevada/events.html'
     model = Event
     context_object_name = 'events'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['wp_integrate'] = WORDPRESS
+        return context
 
 
 class EventView(DetailView):
     template_name = 'exevada/event.html'
     model = Event
     context_object_name = 'event'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['wp_integrate'] = WORDPRESS
+        return context
 
 class AttributionsView(ListView):
     template_name = 'exevada/attributions.html'
@@ -35,28 +47,36 @@ class AttributionsView(ListView):
         geom = serialize('geojson', Location.objects.all(),
                     geometry_field='area',
                     fields=('name','description',))
-        # Add in a QuerySet of all the books
-        #geom = geos.GeometryCollection()
-        #for attribution in self.object_list:
-        #    geom.append(attribution.location.area)
         context['location_geojson'] = geom
+        context['wp_integrate'] = WORDPRESS
         return context
-
 
 
 class AttributionView(DetailView):
     template_name = 'exevada/attribution.html'
     model = Attribution
     context_object_name = 'attribution'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['wp_integrate'] = WORDPRESS
+        return context
 
 
 class ObsDatasets(ListView):
     template_name = 'exevada/obsdatasets.html'
     model = ObservationDataSet
     context_object_name = 'observation_datasets'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['wp_integrate'] = WORDPRESS
+        return context
 
 
 class ModDatasets(ListView):
     template_name = 'exevada/moddatasets.html'
     model = ModelDataSet
     context_object_name = 'model_datasets'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['wp_integrate'] = WORDPRESS
+        return context
